766ae6eb83a04a6c7d8ce834de748f6924be2156
chenzhixiang1992 <chenzhixiang1992@gmail.com>
2017-06-22 20:39:31 +0800

    增加pages.index方法

--- django/myblog/blog/views/pages.py
+++ django/myblog/blog/views/pages.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from blog.models import Article, Comment, Poll, User
 
 def index(request):
-    latest_
+    article_list = Article.objects.query_by_time()
+    # loginform = LoginForm()
+    context = {'article_list': article_list}
+    return render(request, 'index.html', context)
 # Create your views here.
