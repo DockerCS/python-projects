ac30de5d38dcd7cfec908dc8aa25099adb285984
chenzhixiang1992 <chenzhixiang1992@gmail.com>
2017-07-30 16:28:00 +0800

    20170730下午-上一篇和下一篇

--- django/czxblog/apps_project/blog/views.py
+++ django/czxblog/apps_project/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from ..blog.models import Article, Comment, Poll
+from ..blog.models import Article, Comment
 from ..blog.forms import CommentForm
+from ..tools.views.paginate import getPages
+from ..tools.views.view_decorator import record_view
+from ..tools.views.article_recommend import rand_article, pre_next_article
 # from ..user.forms import LoginForm
 
 import markdown2
@@ -8,25 +11,31 @@ import markdown2
 # 主页方法
 def index(request):
     article_list = Article.objects.query_by_time()
+    pages, article_list = getPages(request, article_list)
+
+    data = {}
+    data["article_list"] = article_list
+    data["pages"] = pages
+    data['rand_articles'] = rand_article()
     # loginform = LoginForm()
-    context = {'article_list': article_list}
-    return render(request, 'index.html', context)
+
+    return render(request, 'index.html', data)
 
 # 文章的方法
-def article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
-    content = markdown2.markdown(article.content, extras=["code-friendly", "fenced-code-blocks", "header-ids", "toc", "metadata"])
-    commentform = CommentForm()
-    # loginform = LoginForm()
-    comment = article.comment_set.all
+@record_view(Article)
+def article_show(request, id):
+    data = {}
+    data['article'] = get_object_or_404(Article, id=id)
+    data['content'] = markdown2.markdown(data['article'].content, extras=["code-friendly", "fenced-code-blocks", "header-ids", "toc", "metadata"])
+    data['rand_articles'] = rand_article(id)  # 加入id，并把id传入article_recommend以便排除正在浏览的文章
+    data['pre_article'] = pre_next_article(id)[0]
+    data['next_article'] = pre_next_article(id)[1]
 
-    return render(request, 'blog/article_page.html', {
-        'article': article,
-        # 'loginform': loginform,
-        'commentform': commentform,
-        'content': content,
-        'comment': comment,
-        })
+    # commentform = CommentForm()
+    # loginform = LoginForm()
+    # comment = article.comment_set.all
+
+    return render(request, 'blog/article_page.html', data)
 
 # 评论方法
 def comment(request, article_id):
--- django/czxblog/apps_project/tools/views/article_recommend.py
+++ django/czxblog/apps_project/tools/views/article_recommend.py
@@ -10,3 +10,34 @@ def rand_article(except_id=0):
     """
     rand_num = 10
     return Article.objects.exclude(id=except_id).order_by('?')[:rand_num]
+
+# 上一篇和下一篇文章推荐
+def pre_next_article(id):
+    """
+    在artilce_show中引用pre_next_article方法，并传递参数id(当前文章的id)
+    到pre_next_article方法，然后按照下面的方法得到上一篇和下一篇文章。
+    data['pre_article'] = pre_next_article(id)[0] # 代表引用pre_next_article方法返回的第一个参数
+    data['next_article'] = pre_next_article(id)[1] # 代表引用pre_next_article方法返回的第二个参数
+    """
+    current_article = Article.objects.get(id=id)  # 当前打开的博客
+    """得到当前文章的前面部分和后面部分全部文章，接下来，获取它们的
+    第一条记录，便是上一篇和下一篇文章；其中，__gt和__lt分别是大于和
+    小于的意思，可以修饰到判断条件的字段上。
+    """
+    pre_article = Article.objects.filter(id__gt=current_article.id).order_by('id')
+    next_article = Article.objects.filter(id__lt=current_article.id).order_by('-id')
+
+    # 获取第一条记录
+    if pre_article.count() > 0:
+        pre_article = pre_article[0]
+    else:
+        pre_article = None
+
+    if next_article.count() > 0:
+        next_article = next_article[0]
+    else:
+        next_article = None
+
+    return pre_article, next_article
+
+
